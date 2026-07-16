14011c0d7ff12afb9efcd71b05f7eab8cd852b21
qburi <divyeshbalu@gmail.com>
2026-07-16 12:44:18 +0530

    Resolve test_file.c

---
+++ task_2/test_file.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    a = 10;
+    b = 10;
+    if (a >= b) {
+        printf("a is greater than b");
+    } else {
+        printf("b is greatr than a");
+    }
+    return 0;
+}
